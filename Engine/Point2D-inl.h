07277fa8f5a312375975b1efc8633451887fd55a
Byreave Luo <654297651@qq.com>
2018-10-28 17:35:06 -0600

    add const unittest

--- Engine/Point2D-inl.h
+++ Engine/Point2D-inl.h
@@ -1,7 +1,7 @@
 #pragma once
 namespace Engine {
 	template<typename T>
-	inline bool Point2D<T>::operator==(const Point2D<T>& other)
+	inline bool Point2D<T>::operator==(const Point2D<T>& other) const
 	{
 		return _x == other._x && _y == other._y;
 	}
